88cf5b94a37457410d59e570452a1b031a64205e
Wasif Khan <waasif.khaan@gmail.com>
2018-05-10 17:25:24 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.include?("brand")
+    BRANDS << brand unless BRANDS.detect?("brand")
     end
 
   def cobble
